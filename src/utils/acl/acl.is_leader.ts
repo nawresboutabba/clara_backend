394c2e57d4c9d82046c9780750ea4748fb76f787
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-01-25 15:47:21 -0300

    throw errror replaced.

--- src/utils/acl/acl.is_leader.ts
+++ src/utils/acl/acl.is_leader.ts
@@ -7,13 +7,14 @@ export function IS_LEADER(req: RequestMiddleware): Promise<void>{
     return new Promise(async (resolve, reject)=> {
         try{
             const currentLeader = await IntegrantService.currentLeader() 
-            if(req.user.userId === currentLeader.user.userId){
+            if(req.user.userId == currentLeader?.user?.userId){
                 return resolve()
             }
-            return reject(new RoutingError(
+
+            throw new RoutingError(
                 ERRORS.ROUTING.OPERATION_NOT_AVAILABLE,
                 HTTP_RESPONSE._500
-            ))
+            )
         }catch(error){
             return reject(error)
         }

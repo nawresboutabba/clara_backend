46dfe7355ec8f1bea2e6ab19c4364ac2c187d655
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-01-26 14:27:34 -0300

    new group validator added

--- src/repository/repository.group-validator.ts
+++ src/repository/repository.group-validator.ts
@@ -4,17 +4,39 @@ import GroupValidatorService from "../services/GroupValidator.service";
 import { GroupValidatorBody } from "../controller/group-validator";
 import { GroupValidatorI } from "../models/group-validator";
 import { nanoid } from "nanoid";
+import { IntegrantI } from "../models/integrant";
+import { genericGroupValidatorFilter } from "../utils/field-filters/group-validator";
+
+export interface GroupValidatorResponse {
+    group_validator_id: string,
+    name: string,
+    created: Date,
+}
 
 export const newGroupValidator = async (body:GroupValidatorBody)=> {
     return new Promise(async (resolve, reject)=> {
         try{
+            /**
+             * Create new group validator variable
+             */
             const groupValidator: GroupValidatorI = {
                 groupValidatorId: nanoid(),
                 name: body.name,
-                created: new Date()
+                created: new Date(),
             }
+            /**
+            * Get integrants for new Group Validator
+            */
+            const integrants: IntegrantI [] = await IntegrantService.getAllIntegrantsListById(body.integrants)
+
+            const groupValidatorDocs = await GroupValidatorService.newGroupValidator(groupValidator)
             
-            const resp = await GroupValidatorService.newGroupValidator(groupValidator)
+            /**
+            * Insert integrants to group validator
+            */
+            await IntegrantService.insertIntegrantsToGroupValidator(integrants, groupValidatorDocs)            
+
+            const resp = genericGroupValidatorFilter(groupValidatorDocs)
             return resolve(resp)
         }catch(error){
             return reject(error)

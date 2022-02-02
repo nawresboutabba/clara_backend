cd28f81fce4e9b09c6ad1c9b81353745edb9df0b
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-02 08:51:11 -0300

    to camel case function added

---
+++ src/utils/general/to-camel-case.ts
@@ -0,0 +1,14 @@
+import * as _ from 'lodash';
+
+
+export const toCamelCase = (element: any): any => {
+    /**
+     * Regex expresion that replace all occurences of wsalevel
+     * ignoring between uppercase and lowercase letters
+     */
+    const regexExpresion = new RegExp("wsaLevel", "gi")
+    const response = _.mapKeys(element, (v: any, k:any) => 
+    _.replace(_.camelCase(k), regexExpresion, "WSALevel")
+    );
+    return response;
+}

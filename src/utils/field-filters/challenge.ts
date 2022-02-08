3f3fb9d2dd08938e414ec03f4045462d8e767f4b
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-08 10:00:13 -0300

    configuration adde to filter

--- src/utils/field-filters/challenge.ts
+++ src/utils/field-filters/challenge.ts
@@ -1,7 +1,8 @@
 import { ChallengeResponse } from "../../controller/challenge";
 import { ChallengeI } from "../../models/situation.challenges";
-import { genericAreaFilter, genericArrayAreaFilter } from "./area";
+import { genericArrayAreaFilter } from "./area";
 import { genericUserFilter } from "./user";
+import { genericGroupValidatorFilter } from "./group-validator";
 
 /**
  * Challenge information filter. 
@@ -13,41 +14,84 @@ import { genericUserFilter } from "./user";
 
 export const genericChallengeFilter = async (challenge : ChallengeI): Promise<ChallengeResponse> => {
     return new Promise(async (resolve, reject)=> {
-        const { 
+        const {
+            challengeId, 
             created,
+            status,
+            updated,
             title, 
             description , 
-            status, 
-            updated,
-            images,
-            WSALevel,
+            active,
             fileComplementary,
+            images,
             isStrategic,
-            timePeriod,
-            participationMode,
-            reactions,
-            challengeId
+            finalization,
+            /**
+             * Configuration section
+             */
+            canShowDisagreement,
+            canFixDisapprovedIdea,
+            canChooseScope,
+            isPrivated,
+            canChooseWSALevel,
+            WSALevelAvailable,
+            WSALevelChosed,
+            communityCanSeeReactions,
+            minimumLikes,
+            maximumDontUnderstand,
+            reactionFilter,
+            participationModeAvailable,
+            participationModeChosed,
+            timeInPark,
+            timeExpertFeedback,
+            timeIdeaFix,
+            externalContributionAvailableForGenerators,
+            externalContributionAvailableForCommittee
         } = challenge
         const author = await genericUserFilter(challenge.author)
         const inserted_by = await genericUserFilter(challenge.insertedBy)
-        const areasAvailable = await genericArrayAreaFilter(challenge.areasAvailable)
+        const areas_available = await genericArrayAreaFilter(challenge.areasAvailable)
+        const department_affected = await genericArrayAreaFilter(challenge.departmentAffected)
+        const group_validator = await genericGroupValidatorFilter(challenge.groupValidator)
+        
         return resolve({
-            challenge_id: challengeId,
+            challenge_id: challengeId, 
             inserted_by,
             author,
             created,
+            status,
+            updated,
             title, 
             description , 
-            status, 
-            updated,
-            images,
-            WSALevel,
+            active,
             file_complementary: fileComplementary,
+            images,
             is_strategic: isStrategic,
-            time_period: timePeriod,
-            participation_mode: participationMode,
-            reactions,
-            areas_available: areasAvailable
+            finalization,
+            department_affected,
+            group_validator,
+            /**
+             * Configuration section
+             */
+            can_show_disagreement: canShowDisagreement,
+            can_fix_disapproved_idea: canFixDisapprovedIdea,
+            can_choose_scope: canChooseScope,
+            is_privated: isPrivated,
+            can_choose_WSALevel: canChooseWSALevel,
+            WSALevel_available: WSALevelAvailable,
+            WSALevel_chosed: WSALevelChosed,
+            areas_available,
+            community_can_see_reactions: communityCanSeeReactions,
+            minimum_likes: minimumLikes,
+            maximum_dont_understand: maximumDontUnderstand,
+            reaction_filter: reactionFilter,
+            participation_mode_available: participationModeAvailable,
+            participation_mode_chosed: participationModeChosed,
+            time_in_park: timeInPark,
+            time_expert_feedback: timeExpertFeedback,
+            time_idea_fix: timeIdeaFix,
+            external_contribution_available_for_generators: externalContributionAvailableForGenerators,
+            external_contribution_available_for_committee: externalContributionAvailableForCommittee
         })
     })
 }

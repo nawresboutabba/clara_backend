9002737cca72ad7345b10ccd8dd90705af0f9bc7
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-08 10:01:46 -0300

    configuration fields added

--- src/utils/field-filters/solution.ts
+++ src/utils/field-filters/solution.ts
@@ -7,55 +7,91 @@ import { genericArrayAreaFilter } from "./area";
 import { genericTeamFilter } from "./team";
 import { genericArrayUserFilter, genericUserFilter } from "./user";
 
-export const genericSolutionFilter = async(solution: SolutionI ): Promise<SolutionResponse> => {
-    return new Promise(async (resolve, reject)=> {
+export const genericSolutionFilter = async(solution: SolutionI ): Promise<SolutionResponse> => {  
+  return new Promise(async (resolve, reject)=> {
         const {
             solutionId,
+            challengeId,
             created,
             status,
             updated,
             title,
             description,
-            images,
-            WSALevel,
+            active,
             fileComplementary,
-            isPrivate,
+            images,
+            /**
+             * Configuration section
+             */
+            canShowDisagreement,
+            canFixDisapprovedIdea,
+            canChooseScope,
+            isPrivated,
+            canChooseWSALevel,
+            WSALevelAvailable,
+            WSALevelChosed,
+            communityCanSeeReactions,
+            minimumLikes,
+            maximumDontUnderstand,
+            reactionFilter,
+            participationModeAvailable,
+            participationModeChosed,
             timeInPark,
-            reactions,
-            challengeId,
-            participationModeChosen
+            timeExpertFeedback,
+            timeIdeaFix,
+            externalContributionAvailableForGenerators,
+            externalContributionAvailableForCommittee
         } = solution
 
         let coauthor : UserResponse[]
         let team : TeamResponse
-        if(solution.participationModeChosen == PARTICIPATION_MODE.INDIVIDUAL_WITH_COAUTHORSHIP){
+        if(solution.participationModeChosed == PARTICIPATION_MODE.INDIVIDUAL_WITH_COAUTHORSHIP){
             coauthor = await genericArrayUserFilter(solution.coauthor)
-        }else if (solution.participationModeChosen == PARTICIPATION_MODE.TEAM){
+        }else if (solution.participationModeChosed == PARTICIPATION_MODE.TEAM){
             team = await genericTeamFilter(solution.team)
         }
         const author = await genericUserFilter(solution.author)
         const inserted_by = await genericUserFilter(solution.insertedBy)
         const areasAvailable = await genericArrayAreaFilter(solution.areasAvailable)
+        const departmentAffected = await genericArrayAreaFilter(solution.departmentAffected)
         return resolve({
             inserted_by,
             author,
             coauthor,
             team,
+            active,
             solution_id:solutionId,
+            challenge_id:challengeId,
             created,
             status,
             updated,
+            file_complementary:fileComplementary,
             title,
             description,
             images,
-            WSALevel,
-            file_complementary: fileComplementary,
-            is_private: isPrivate,
-            time_in_park: timeInPark,
-            reactions,
-            challenge_id:challengeId,
+            department_affected:departmentAffected,
+            /**
+             * Configuration section
+             */
+            can_show_disagreement: canShowDisagreement,
+            can_fix_disapproved_idea: canFixDisapprovedIdea,
+            can_choose_scope: canChooseScope,
+            is_privated: isPrivated,
+            can_choose_WSALevel: canChooseWSALevel,
+            WSALevel_available: WSALevelAvailable,
+            WSALevel_chosed: WSALevelChosed,
             areas_available: areasAvailable,
-            participation_mode_choosed: participationModeChosen            
+            community_can_see_reactions: communityCanSeeReactions,
+            minimum_likes: minimumLikes,
+            maximum_dont_understand: maximumDontUnderstand,
+            reaction_filter: reactionFilter,
+            participation_mode_available: participationModeAvailable,
+            participation_mode_chosed: participationModeChosed,
+            time_in_park: timeInPark,
+            time_expert_feedback: timeExpertFeedback,
+            time_idea_fix: timeIdeaFix,           
+            external_contribution_available_for_generators: externalContributionAvailableForGenerators,
+            external_contribution_available_for_committee: externalContributionAvailableForCommittee,
         })
     })
 }

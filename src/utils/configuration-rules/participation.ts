b27b310232706f1fd32a445b30fd8a8131090d5e
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-23 16:34:07 -0300

    identation

--- src/utils/configuration-rules/participation.ts
+++ src/utils/configuration-rules/participation.ts
@@ -3,45 +3,45 @@ import { UserI } from "../../models/users"
 import { isCommitteMember } from "../acl/function.is_committe_member"
 
 export const isCompositionUsersValid = (author: UserI, coauthor:UserI[], challenge: ChallengeI): Promise<boolean> => {
-    return new Promise(async (resolve, reject)=> {
-        let  areThereExternalsUser: boolean = false
+  return new Promise(async (resolve, reject)=> {
+    let  areThereExternalsUser = false
 
-        coauthor.forEach(user => {
-            if(user.externalUser === true) {
-                areThereExternalsUser = true
-            }
-        })
+    coauthor.forEach(user => {
+      if(user.externalUser === true) {
+        areThereExternalsUser = true
+      }
+    })
 
-        if(!areThereExternalsUser){
-            return resolve(true)
-        }else{
-            const committeeMember = await isCommitteMember(author)
+    if(!areThereExternalsUser){
+      return resolve(true)
+    }else{
+      const committeeMember = await isCommitteMember(author)
 
-            /**
+      /**
              * User can invite external users like committee member or generator
              */
-            if(challenge.externalContributionAvailableForCommittee 
+      if(challenge.externalContributionAvailableForCommittee 
                 && challenge.externalContributionAvailableForGenerators){
-                return resolve(true)
-            /**
+        return resolve(true)
+        /**
              * User can not invite external users like committee member or generator
              */
-            } else if(!challenge.externalContributionAvailableForCommittee 
+      } else if(!challenge.externalContributionAvailableForCommittee 
                 && !challenge.externalContributionAvailableForGenerators){
-                return resolve(false)
-            /**
+        return resolve(false)
+        /**
              * User can invite external users like committee member
              */
-            }else if(challenge.externalContributionAvailableForCommittee && committeeMember.isActive){
-                return resolve(true)
-            /**
+      }else if(challenge.externalContributionAvailableForCommittee && committeeMember.isActive){
+        return resolve(true)
+        /**
              * User can invite external users like generator
              */
-            } else if(challenge.externalContributionAvailableForGenerators && !committeeMember.isActive){
-                return resolve(true)
-            }else {
-                return resolve(false)
-            }
-        }
-    })
+      } else if(challenge.externalContributionAvailableForGenerators && !committeeMember.isActive){
+        return resolve(true)
+      }else {
+        return resolve(false)
+      }
+    }
+  })
 }

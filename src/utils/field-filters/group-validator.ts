925fc8976840441da467a8fd9afe753f65db2012
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-23 16:28:58 -0300

    identation

--- src/utils/field-filters/group-validator.ts
+++ src/utils/field-filters/group-validator.ts
@@ -2,19 +2,19 @@ import { GroupValidatorI } from "../../models/group-validator";
 import { GroupValidatorResponse } from "../../repository/repository.group-validator";
 
 export const genericGroupValidatorFilter = async (groupValidator: GroupValidatorI): Promise<GroupValidatorResponse> => {
-    return new Promise(async (resolve, reject) => {
-        if(!groupValidator){
-            return resolve(undefined)
-        }
-        const {
-            groupValidatorId,
-            name,
-            created
-        } = groupValidator
-        return resolve({
-            group_validator_id: groupValidatorId,
-            name,
-            created
-        })
+  return new Promise(async (resolve, reject) => {
+    if(!groupValidator){
+      return resolve(undefined)
+    }
+    const {
+      groupValidatorId,
+      name,
+      created
+    } = groupValidator
+    return resolve({
+      group_validator_id: groupValidatorId,
+      name,
+      created
     })
+  })
 }

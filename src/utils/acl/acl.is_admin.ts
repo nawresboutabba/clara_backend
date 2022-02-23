5293a5296ec0a0801192f2b7ed6a5220b5ec7239
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-23 16:36:19 -0300

    identation

--- src/utils/acl/acl.is_admin.ts
+++ src/utils/acl/acl.is_admin.ts
@@ -3,17 +3,17 @@ import { ERRORS, HTTP_RESPONSE, OWNER } from "../../constants";
 import RoutingError from "../../handle-error/error.routing";
 
 export function IS_ADMIN(req: RequestMiddleware): Promise<void>{
-    return new Promise((resolve, reject)=> {
-        try{
-            if(req.user.email === OWNER){
-                return resolve()
-            }
-            return reject(new RoutingError(
-                ERRORS.ROUTING.OPERATION_NOT_AVAILABLE,
-                HTTP_RESPONSE._500
-            ))
-        }catch(error){
-            return reject(error)
-        }
-    })
+  return new Promise((resolve, reject)=> {
+    try{
+      if(req.user.email === OWNER){
+        return resolve()
+      }
+      return reject(new RoutingError(
+        ERRORS.ROUTING.OPERATION_NOT_AVAILABLE,
+        HTTP_RESPONSE._500
+      ))
+    }catch(error){
+      return reject(error)
+    }
+  })
 }

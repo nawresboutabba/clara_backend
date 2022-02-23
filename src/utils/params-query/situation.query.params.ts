d5e6f55fc1414a63787c9f77065daeed2659f33d
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-23 16:20:27 -0300

    identation, query optimized

--- src/utils/params-query/situation.query.params.ts
+++ src/utils/params-query/situation.query.params.ts
@@ -1,63 +1,63 @@
-import checkISOData from "../date";
 import * as _ from 'lodash'
+import { getCurrentDate } from '../date';
 
 export interface QuerySituationForm {
-    created?: createdFilter,
-    init?: number,
-    offset?: number,
-    sort?: {
-        title:string,
-        created: string    
-    }
-    title?: string
+  created?: createdFilter,
+  init?: number,
+  offset?: number,
+  sort?: {
+    title: string,
+    created: string
+  }
+  title?: string
 }
 
 export interface createdFilter {
-    $lte?:Date,
-    $gte?: Date
+  $lte?: Date,
+  $gte?: Date
 }
 
-export function formatSitutationQuery(query: any): Promise<QuerySituationForm> {
-    return new Promise((resolve, reject)=> {
-        /**
-         * Pagination extraction and cleaning
-         */
-         let { init, offset } = query
-         init = parseInt(query.init.toString()) || 0;
-         offset = parseInt(query.offset.toString()) || 10;
-         /**
-          * Date filter
-          */
-         let { created_lt: $lte, created_gt: $gte }= query
+export async function formatSitutationQuery(query: any): Promise<QuerySituationForm> {
+  try {
+    /**
+      * Pagination extraction and cleaning
+      */
+    let { init, offset } = query
+    init = parseInt(query.init.toString()) || 0;
+    offset = parseInt(query.offset.toString()) || 10;
+    /**
+      * Date filter
+      */
+    const { created_lt: $lte, created_gt: $gte } = query
 
-         let created: createdFilter ={}
-         created.$gte= $gte
-         created.$lte = $lte
+    const created: createdFilter = {}
+    created.$gte = $gte 
+    created.$lte = $lte
 
-         /**
-          * String filters
-          */
-          const { title  }= query     
-          /**
-           * Order filter
-           */
-          const { created_order, title_order }= query
+    /**
+      * String filters
+      */
+    const { title } = query
+    /**
+      * Order filter
+      */
+    const { created_order, title_order } = query
 
-          const queryForm: QuerySituationForm = {
-              init,
-              offset,
-              sort :{
-                  title: title_order,
-                  created: created_order
-              },
-          }
-            
-          queryForm.title = title ? title : ''         
- 
+    let queryForm: QuerySituationForm = {
+      init,
+      offset,
+      sort: {
+        title: title_order,
+        created: created_order
+      },
+    }
 
-          if (!(_.isEmpty(created))){
-            queryForm.created = created
-          }
-          return resolve(queryForm)
-    })
+    queryForm.title = title ? title : ''
+
+    queryForm = JSON.parse(JSON.stringify(queryForm))
+
+    return queryForm
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }

b5a1f4e536778d02fd4fbd0044932507bc017da5
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-23 17:01:12 -0300

    identation

--- src/routes/integrant/index.ts
+++ src/routes/integrant/index.ts
@@ -12,100 +12,104 @@ import { throwSanitizatorErrors } from "../../utils/sanitization/satitization.er
 const router = require("express").Router();
 
 router.get('/integrant/general',[
-    authentication
+  authentication
 ], async (req: RequestMiddleware, res: ResponseMiddleware, next: NextFunction)=> {
-    try{
-        const integrantController = new IntegrantController()
-        const generalMember =  await integrantController.getGeneralMembers()
-        res
-        .json(generalMember)
-        .status(200)
-        .send()
-    }catch(error){
-        next(error)
-    }
+  try{
+    const integrantController = new IntegrantController()
+    const generalMember =  await integrantController.getGeneralMembers()
+    res
+      .json(generalMember)
+      .status(200)
+      .send()
+  }catch(error){
+    next(error)
+  }
 })
 
 router.post('/integrant/general',[
-    authentication,
-    acl(
-        RULES.IS_LEADER
-      ),
-    check('userId',"user does not exist").custom((value, {req}): Promise<void>=> {
-        return new Promise(async (resolve, reject)=> {
-            const user = await UserService.getUserActiveByUserId(value)
-            if(!user){
-                return reject()
-            }
-            return resolve()
-        })
-    })
-],async (req: RequestMiddleware, res: ResponseMiddleware, next: NextFunction)=> {
+  authentication,
+  acl(
+    RULES.IS_LEADER
+  ),
+  check('userId',"user does not exist").custom(async (value, {req}): Promise<void>=> {
     try{
-        await throwSanitizatorErrors(validationResult , req, ERRORS.ROUTING.SIGNUP_USER)
-        const integrantController = new IntegrantController()
-        const integrant = await integrantController.newIntegrant(req.body)
-        res
-        .json(integrant)
-        .send()
+      const user = await UserService.getUserActiveByUserId(value)
+      if(!user){
+        return Promise.reject()
+      }
+      req.utils = {...req.utils,user}
+      return Promise.resolve()
     }catch(error){
-        next(error)
+      return Promise.reject()
     }
+  })
+],async (req: RequestMiddleware, res: ResponseMiddleware, next: NextFunction)=> {
+  try{
+    await throwSanitizatorErrors(validationResult , req, ERRORS.ROUTING.SIGNUP_USER)
+    const integrantController = new IntegrantController()
+    const integrant = await integrantController.newIntegrant(req.utils.user.userId, req.utils.user) 
+    res
+      .json(integrant)
+      .status(200)
+      .send()
+  }catch(error){
+    next(error)
+  }
 })
 
 router.post('/integrant/leader/:integrantId',[
-    authentication,
-    acl(
-        RULES.IS_ADMIN
-      ),
-    check('integrantId', "integrant does not exist").custom((value, {req}): Promise<void>=> {
-        return new Promise(async (resolve, reject)=> {
-            const check = await IntegrantService.checkIntegrantStatus(value)
-            if(check.exist && check.isActive){
-                return resolve()
-            }
-            return reject()
-        })
-    }),
+  authentication,
+  acl(
+    RULES.IS_ADMIN
+  ),
+  check('integrantId', "integrant does not exist").custom((value, {req}): Promise<void>=> {
+    return new Promise(async (resolve, reject)=> {
+      const check = await IntegrantService.checkIntegrantStatus(value)
+      if(check.exist && check.isActive){
+        return resolve()
+      }
+      return reject()
+    })
+  }),
 ],async (req: RequestMiddleware, res: ResponseMiddleware, next: NextFunction)=> {
-    try{
-        await throwSanitizatorErrors(validationResult , req, ERRORS.ROUTING.SIGNUP_USER)
+  try{
+    await throwSanitizatorErrors(validationResult , req, ERRORS.ROUTING.SIGNUP_USER)
 
-        const integrantController = new IntegrantController()
-        const integrant = await integrantController.newLeader(req.params.integrantId)
-        res
-        .json(integrant)
-        .send()
-    }catch(error){
-        next(error)
-    }
+    const integrantController = new IntegrantController()
+    const integrant = await integrantController.newLeader(req.params.integrantId)
+    res
+      .json(integrant)
+      .send()
+  }catch(error){
+    next(error)
+  }
 })
 
 
 router.delete('/integrant/general/:integrantId',[
-    authentication,
-    acl(
-        RULES.IS_LEADER
-      ),
-    check('integrantId', "integrant does not exist").custom((value, {req}): Promise<void>=> {
-        return new Promise(async (resolve, reject)=> {
-            const check = await IntegrantService.checkIntegrantStatus(value)
-            if(check.exist && check.isActive){
-                return resolve()
-            }
-            return reject()
-        })
-    }),
+  authentication,
+  acl(
+    RULES.IS_LEADER
+  ),
+  check('integrantId', "integrant does not exist").custom((value, {req}): Promise<void>=> {
+    return new Promise(async (resolve, reject)=> {
+      const check = await IntegrantService.checkIntegrantStatus(value)
+      if(check.exist && check.isActive){
+        return resolve()
+      }
+      return reject()
+    })
+  }),
 ],async (req: RequestMiddleware,res: ResponseMiddleware,next: NextFunction)=> {
-    try{
-        const integrantController = new IntegrantController()
-        const integrant = await integrantController.deleteGeneralMember(req.params.integrantId)
-        res
-        .json(integrant)
-        .send()
-    }catch(error){
-        next(error)
-    }
+  try{
+    const integrantController = new IntegrantController()
+    const integrant = await integrantController.deleteGeneralMember(req.params.integrantId)
+    res
+      .json(integrant)
+      .send()
+  }catch(error){
+    next(error)
+  }
 })
 const integrantRouter = router
 

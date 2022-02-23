596c31836bac1a24387336e75b0d482e2aa70fca
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-23 17:02:20 -0300

    identation

--- src/routes/company/index.ts
+++ src/routes/company/index.ts
@@ -8,29 +8,29 @@ import { validationResult, body } from "express-validator";
 const router = require("express").Router();
 
 router.post ('/company',[
-    body('CNPJ', 'CNPJ can not be empty').notEmpty(),
-    body('name', 'name can not be empty').notEmpty()
-    ],
- async (req: RequestMiddleware,res:ResponseMiddleware,next: NextFunction) => {
-    try{
-        const errors = validationResult(req).array();
+  body('CNPJ', 'CNPJ can not be empty').notEmpty(),
+  body('name', 'name can not be empty').notEmpty()
+],
+             async (req: RequestMiddleware,res:ResponseMiddleware,next: NextFunction) => {
+               try{
+                 const errors = validationResult(req).array();
   
-        if (errors.length > 0) {
-          res.status(400);
-          throw new Error(JSON.stringify(errors));
-        }   
+                 if (errors.length > 0) {
+                   res.status(400);
+                   throw new Error(JSON.stringify(errors));
+                 }   
 
-        const companyController = new CompanyController()
-        let resp: CompanyI
-        resp = await companyController.newCompany(req.body)
-        res
-            .status(200)
-            .json(resp)
-            .send()
-    }catch(error){
-        next(error)
-    }
-})
+                 const companyController = new CompanyController()
+                 let resp: CompanyI
+                 resp = await companyController.newCompany(req.body)
+                 res
+                   .status(200)
+                   .json(resp)
+                   .send()
+               }catch(error){
+                 next(error)
+               }
+             })
 const companyRouter = router
 
 export default companyRouter

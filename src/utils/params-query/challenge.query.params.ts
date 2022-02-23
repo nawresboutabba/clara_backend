8d4e560ebf7a1e9f059bf0b006248b8f9d8efb73
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-23 16:21:04 -0300

    identation, query optimized

--- src/utils/params-query/challenge.query.params.ts
+++ src/utils/params-query/challenge.query.params.ts
@@ -2,14 +2,14 @@ import { PARTICIPATION_MODE } from "../../constants";
 import { QuerySituationForm, formatSitutationQuery } from "./situation.query.params";
 
 function modalitySwitch(modality:string){
-    switch(modality){
-        case '0': 
-            return [PARTICIPATION_MODE.TEAM]
-        case '1':
-            return [PARTICIPATION_MODE.INDIVIDUAL_WITH_COAUTHORSHIP]
-        default:
-            return [PARTICIPATION_MODE.TEAM,PARTICIPATION_MODE.INDIVIDUAL_WITH_COAUTHORSHIP]
-    }
+  switch(modality){
+  case '0': 
+    return [PARTICIPATION_MODE.TEAM]
+  case '1':
+    return [PARTICIPATION_MODE.INDIVIDUAL_WITH_COAUTHORSHIP]
+  default:
+    return [PARTICIPATION_MODE.TEAM,PARTICIPATION_MODE.INDIVIDUAL_WITH_COAUTHORSHIP]
+  }
 }
 
 export interface QueryChallengeForm extends QuerySituationForm {
@@ -17,27 +17,29 @@ export interface QueryChallengeForm extends QuerySituationForm {
     participationMode?: Array<string>
 }
 
-export function formatChallengeQuery(query:any): Promise<QueryChallengeForm>{
-    return new Promise(async (resolve, reject)=> {
-        const querySituationForm:QuerySituationForm = await formatSitutationQuery(query)
-        let { 
-            is_strategic,
-            participation_mode
-        } = query
-
-        let queryChallengeForm: QueryChallengeForm = {
-            ... querySituationForm,
-        }
-
-        if (is_strategic){
-            const isStrategic: boolean = (is_strategic != undefined && is_strategic.toLowerCase() == 'true') ? true : false;
-            queryChallengeForm.isStrategic = isStrategic
-        }
-        
-        const participationMode = modalitySwitch(participation_mode)
-
-        queryChallengeForm.participationMode = participationMode
-
-        return resolve(queryChallengeForm)
-    })
+export async function formatChallengeQuery(query:any): Promise<QueryChallengeForm>{
+  try{
+    const querySituationForm:QuerySituationForm = await formatSitutationQuery(query)
+    const { 
+      is_strategic,
+      participation_mode
+    } = query
+    
+    const queryChallengeForm: QueryChallengeForm = {
+      ... querySituationForm,
+    }
+    
+    if (is_strategic){
+      const isStrategic: boolean = (is_strategic != undefined && is_strategic.toLowerCase() == 'true') ? true : false;
+      queryChallengeForm.isStrategic = isStrategic
+    }
+            
+    const participationMode = modalitySwitch(participation_mode)
+    
+    queryChallengeForm.participationMode = participationMode
+    
+    return queryChallengeForm
+  }catch(error){
+    return Promise.reject(error)
+  }
 }

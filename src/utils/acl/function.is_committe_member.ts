3090c2aeebbb183996f179408fcdf1343373e286
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-23 16:34:37 -0300

    identation

--- src/utils/acl/function.is_committe_member.ts
+++ src/utils/acl/function.is_committe_member.ts
@@ -3,8 +3,8 @@ import { UserI } from "../../models/users";
 import IntegrantService from "../../services/Integrant.service";
 
 export function isCommitteMember (user: UserI):Promise<IntegrantStatusI>{
-    return new Promise(async (resolve, reject)=> {
-        const committeMember = await IntegrantService.checkUserInCommitte(user.userId)
-        return resolve(committeMember)
-    })
+  return new Promise(async (resolve, reject)=> {
+    const committeMember = await IntegrantService.checkUserInCommitte(user)
+    return resolve(committeMember)
+  })
 }

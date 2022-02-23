bce8c98bd7809fa959a3cac279d7f0d4c46b8c49
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-23 17:01:52 -0300

    identation

--- src/routes/group-validator/index.ts
+++ src/routes/group-validator/index.ts
@@ -13,74 +13,74 @@ import authentication from "../../middlewares/authentication";
 const router = require("express").Router();
 
 router.post('/group-validator',[
-    authentication,
-    body("name", "name can not be empty").notEmpty(),
-    body("name","group validator name already exist").custom(async (value, {req}): Promise<void> => {
-        return new Promise(async (resolve, reject)=> {
-            const groupValidators = await GroupValidatorService.getAllGroupValidators();
-            groupValidators.forEach(groupValidator => {
-                if(groupValidator.name === value){
-                    return reject()
-                }
-            })
-            return resolve()
-        })
-    }),
-    body("integrants").custom(async (value, {req}): Promise<void> => {
-        return new Promise(async (resolve, reject)=> {
+  authentication,
+  body("name", "name can not be empty").notEmpty(),
+  body("name","group validator name already exist").custom(async (value, {req}): Promise<void> => {
+    return new Promise(async (resolve, reject)=> {
+      const groupValidators = await GroupValidatorService.getAllGroupValidators();
+      groupValidators.forEach(groupValidator => {
+        if(groupValidator.name === value){
+          return reject()
+        }
+      })
+      return resolve()
+    })
+  }),
+  body("integrants").custom(async (value, {req}): Promise<void> => {
+    return new Promise(async (resolve, reject)=> {
 
-            /**
+      /**
              * Check that array of integrants is valid
              */
-            if (!_.isArray(value) || value.length === 0) {
-                return reject(ERRORS.ROUTING.INTEGRANTS_MUST_BE_AN_ARRAY)
-            }
-            const integrants = await IntegrantService.getAllIntegrantsListById(value)
-                /**
+      if (!_.isArray(value) || value.length === 0) {
+        return reject(ERRORS.ROUTING.INTEGRANTS_MUST_BE_AN_ARRAY)
+      }
+      const integrants = await IntegrantService.getAllIntegrantsListById(value)
+      /**
                  * Check that integrants does not have group validator
                  */
-                 integrants.forEach(async (integrant:IntegrantI)=> {
-                    if(integrant.groupValidator){
-                        return reject(ERRORS.ROUTING.ONE_OR_MORE_INTEGRANT_IS_ALREADY_IN_GROUP_VALIDATOR)
-                    }
-                })
+      integrants.forEach(async (integrant:IntegrantI)=> {
+        if(integrant.groupValidator){
+          return reject(ERRORS.ROUTING.ONE_OR_MORE_INTEGRANT_IS_ALREADY_IN_GROUP_VALIDATOR)
+        }
+      })
 
-                /**
+      /**
                  * Check that all integrants are available
                  */
-                if (integrants.length !== value.length) {
-                    return reject(ERRORS.ROUTING.ONE_OR_MORE_INTEGRANT_DOES_NOT_EXIST)    
-                }
-                return resolve()
-        })
+      if (integrants.length !== value.length) {
+        return reject(ERRORS.ROUTING.ONE_OR_MORE_INTEGRANT_DOES_NOT_EXIST)    
+      }
+      return resolve()
     })
+  })
 ],async (req: RequestMiddleware, res: ResponseMiddleware, next: NextFunction)=> {
-    try{
-        await throwSanitizatorErrors(validationResult , req, ERRORS.ROUTING.GROUP_VALIDATOR_POST)
+  try{
+    await throwSanitizatorErrors(validationResult , req, ERRORS.ROUTING.GROUP_VALIDATOR_POST)
 
-        const groupValidatorController = new GroupValidatorController()
-        const committe = await groupValidatorController.newGroupValidator(req.body)
-        res
-        .json(committe)
-        .send()
-    }catch(error){
-        next(error)
-    }
+    const groupValidatorController = new GroupValidatorController()
+    const committe = await groupValidatorController.newGroupValidator(req.body)
+    res
+      .json(committe)
+      .send()
+  }catch(error){
+    next(error)
+  }
 })
 
 router.get('/group-validator',[
-    authentication
+  authentication
 ], async (req: RequestMiddleware, res: ResponseMiddleware, next: NextFunction)=> { 
-    try{
-        const groupValidatorController = new GroupValidatorController()
-        const groupValidators = await groupValidatorController.getAllGroupValidatorsDetails()
-        res
-        .json(groupValidators)
-        .status(200)
-        .send()
-    }catch(error){
-        next(error)
-    }
+  try{
+    const groupValidatorController = new GroupValidatorController()
+    const groupValidators = await groupValidatorController.getAllGroupValidatorsDetails()
+    res
+      .json(groupValidators)
+      .status(200)
+      .send()
+  }catch(error){
+    next(error)
+  }
 })
 
 const groupValidatorRouter = router

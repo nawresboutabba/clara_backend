b4871dfa4809d0606e698ed1ee27f62313676aa3
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-02-23 16:27:56 -0300

    identation

--- src/utils/field-filters/reaction.ts
+++ src/utils/field-filters/reaction.ts
@@ -3,16 +3,16 @@ import { ReactionI } from "../../models/interaction.reaction";
 import { genericUserFilter } from "./user";
 
 export const genericReactionFilter = async (reaction: ReactionI): Promise<ReactionResponse> => {
-    return new Promise(async (resolve, reject)=> {
-        const {
-            type,
-            date,
-        } = reaction
-        const author = await genericUserFilter(reaction.author)
-        return resolve({
-            type, 
-            date,
-            author
-        })
+  return new Promise(async (resolve, reject)=> {
+    const {
+      type,
+      date,
+    } = reaction
+    const author = await genericUserFilter(reaction.author)
+    return resolve({
+      type, 
+      date,
+      author
     })
+  })
 }

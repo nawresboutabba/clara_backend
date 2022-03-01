679ac8d6725df02598e8ab8e44021b3ae5e2c19d
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-03-01 14:46:53 -0300

    creator and members deleted from model. recicled author and coauthor field for log member's team

--- src/utils/field-filters/team.ts
+++ src/utils/field-filters/team.ts
@@ -1,35 +1,26 @@
 import { TeamResponse } from "../../controller/team";
 import { TeamI } from "../../models/team";
-import { genericArrayUserFilter, genericUserFilter } from "./user";
 
-export const genericTeamFilter= async (team: TeamI): Promise<TeamResponse> => {
-  return new Promise(async (resolve, reject)=> {
-    const {
-      teamId,
-      created,
-      name,
-    } = team
-    const creator = await genericUserFilter(team.creator) 
-    const members = await genericArrayUserFilter(team.members)
-    return resolve({
-      team_id: teamId,
-      created,
-      name,
-      members,
-      creator
-    })
-  })
+export const genericTeamFilter = async (team: TeamI): Promise<TeamResponse> => {
+  const {
+    teamId,
+    created,
+    name,
+  } = team
+  return {
+    team_id: teamId,
+    created,
+    name,
+  }
 }
 
 export const lightTeamFilter = async (team: TeamI): Promise<any> => {
-  return new Promise(async (resolve, reject)=> {
-    const {
-      teamId,
-      name,
-    } = team
-    return resolve({
-      team_id: teamId,
-      name,
-    })
-  })
+  const {
+    teamId,
+    name,
+  } = team
+  return {
+    team_id: teamId,
+    name,
+  }
 }

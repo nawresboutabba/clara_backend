346ba7cfe0a3cbba2af204cf01c2130dc6f3e3fe
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-03-01 14:48:32 -0300

    identation. parameter default field depends on solution or challenge fixed

--- src/utils/configuration-rules/participation.ts
+++ src/utils/configuration-rules/participation.ts
@@ -1,47 +1,61 @@
+import { ConfigurationDefaultI } from "../../models/configuration.default"
 import { ChallengeI } from "../../models/situation.challenges"
 import { UserI } from "../../models/users"
 import { isCommitteMember } from "../acl/function.is_committe_member"
 
-export const isCompositionUsersValid = (author: UserI, coauthor:UserI[], challenge: ChallengeI): Promise<boolean> => {
-  return new Promise(async (resolve, reject)=> {
-    let  areThereExternalsUser = false
+/**
+ * This functions is used for checking users external participation according to conditions defined in the challenge or default configuration solution
+ * @param author or teamCreator
+ * @param coauthor or members
+ * @param configuration 
+ * @returns 
+ */
+export const isCompositionUsersValid = async (author: UserI, coauthor: UserI[], configuration: ChallengeI | ConfigurationDefaultI ): Promise<boolean> => {
+  let areThereExternalsUser = false
 
-    coauthor.forEach(user => {
-      if(user.externalUser === true) {
-        areThereExternalsUser = true
-      }
-    })
-
-    if(!areThereExternalsUser){
-      return resolve(true)
-    }else{
-      const committeeMember = await isCommitteMember(author)
-
-      /**
-             * User can invite external users like committee member or generator
-             */
-      if(challenge.externalContributionAvailableForCommittee 
-                && challenge.externalContributionAvailableForGenerators){
-        return resolve(true)
-        /**
-             * User can not invite external users like committee member or generator
-             */
-      } else if(!challenge.externalContributionAvailableForCommittee 
-                && !challenge.externalContributionAvailableForGenerators){
-        return resolve(false)
-        /**
-             * User can invite external users like committee member
-             */
-      }else if(challenge.externalContributionAvailableForCommittee && committeeMember.isActive){
-        return resolve(true)
-        /**
-             * User can invite external users like generator
-             */
-      } else if(challenge.externalContributionAvailableForGenerators && !committeeMember.isActive){
-        return resolve(true)
-      }else {
-        return resolve(false)
-      }
+  /**
+   * Check if externals user was invited to compose the team for challenge
+   */
+  coauthor.forEach(user => {
+    if (user.externalUser === true) {
+      areThereExternalsUser = true
     }
   })
+
+  if (!areThereExternalsUser) {
+    /**
+     * If don't have externals user, composition is ok. Don't need more validations. 
+     */
+    return Promise.resolve(true)
+  } else {
+    const committeeMember = await isCommitteMember(author)
+    /**
+      * User can invite external users like committee member or generator
+      */
+    if (configuration.externalContributionAvailableForCommittee
+      && configuration.externalContributionAvailableForGenerators) {
+      return Promise.resolve(true)
+      /**
+        * User can not invite external users like committee member or generator
+        */
+    } else if (!configuration.externalContributionAvailableForCommittee
+      && !configuration.externalContributionAvailableForGenerators) {
+      return Promise.resolve(false)
+      /**
+        * User can invite external users like committee member
+        */
+    } else if (configuration.externalContributionAvailableForCommittee && committeeMember.isActive) {
+      return Promise.resolve(true)
+      /**
+        * User can invite external users like generator
+        */
+    } else if (configuration.externalContributionAvailableForGenerators && !committeeMember.isActive) {
+      return Promise.resolve(true)
+    } else {
+      /**
+       * User can not invite external users. Don't matter the role(committee member or generator).
+       */
+      return Promise.resolve(false)
+    }
+  }
 }

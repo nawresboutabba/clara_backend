45c63a469ce42dcf8c28fde5f02226216e70da4b
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-03-01 14:49:20 -0300

    identation fixed

--- src/utils/acl/acl.can_view_solution.ts
+++ src/utils/acl/acl.can_view_solution.ts
@@ -5,51 +5,49 @@ import { SolutionI } from "../../models/situation.solutions";
 import SolutionService from "../../services/Solution.service";
 import * as _ from 'lodash'; 
 
-export function CAN_VIEW_SOLUTION (req: RequestMiddleware) : Promise<void>{
-  return new Promise(async (resolve, reject)=> {
-    try{
-      if (!(req.resources?.solution)){
-        const solution: SolutionI = await SolutionService.getSolutionActiveById(req.params.solutionId)
-        if(solution == null){
-          return reject(new RoutingError(
-            ERRORS.ROUTING.SOLUTION_FORBIDDEN,
-            HTTP_RESPONSE._500
-          ))                    
-        }
-        req.resources = {... req.resources, solution}
+export async function CAN_VIEW_SOLUTION (req: RequestMiddleware) : Promise<void>{
+  try{
+    const solution: SolutionI = await SolutionService.getSolutionActiveById(req.params.solutionId)
+    if (!(req.resources?.solution)){
+      if(solution == null){
+        return Promise.reject(new RoutingError(
+          ERRORS.ROUTING.SOLUTION_FORBIDDEN,
+          HTTP_RESPONSE._500
+        ))                    
       }
-      const user = req.user
-      if (user.externalUser){
-        /**
-                 * Temporaly: Return error if a external User want to see a challenge
-                 * @TODO create a whitelist for manage external users
-                 */
-        return reject(new RoutingError(
+      req.resources = {... req.resources, solution}
+    }
+    const user = req.user
+    if (user.externalUser){
+      /**
+        * Temporaly: Return error if a external User want to see a challenge
+        * @TODO create a whitelist for manage external users
+        */
+      return Promise.reject(new RoutingError(
+        ERRORS.ROUTING.SOLUTION_FORBIDDEN,
+        HTTP_RESPONSE._500
+      ))
+    } else {
+      if (req.resources.solution.WSALevelChosed == WSALEVEL.AREA){
+        const userAreaVisible = user.areaVisible.filter((area)=> {return area.areaId})
+        const solutionAreasAvailable = req.resources.solution.areasAvailable.filter((area)=> {return area.areaId})
+        const intersection = _.intersection(solutionAreasAvailable, userAreaVisible)
+    
+        if (intersection.length > 0){
+          return Promise.resolve()
+        }
+        return Promise.reject(new RoutingError(
           ERRORS.ROUTING.SOLUTION_FORBIDDEN,
           HTTP_RESPONSE._500
         ))
-      } else {
-        if (req.resources.solution.WSALevelChosed == WSALEVEL.AREA){
-          const userAreaVisible = user.areaVisible.filter((area)=> {return area.areaId})
-          const solutionAreasAvailable = req.resources.solution.areasAvailable.filter((area)=> {return area.areaId})
-          const intersection = _.intersection(solutionAreasAvailable, userAreaVisible)
-    
-          if (intersection.length > 0){
-            return resolve()
-          }
-          return reject(new RoutingError(
-            ERRORS.ROUTING.SOLUTION_FORBIDDEN,
-            HTTP_RESPONSE._500
-          ))
-        }else{
-          /**
-                     * Challenge WSALevel = "COMPANY"
-                     */
-          return resolve()
-        }
+      }else{
+        /**
+          * Challenge WSALevel = "COMPANY"
+          */
+        return Promise.resolve()
       }
-    }catch(error){
-      reject(error)
     }
-  })
+  }catch(error){
+    return Promise.reject(error)
+  }
 }

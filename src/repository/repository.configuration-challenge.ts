e5c676ed60f2a28672221bfb445edb6adc084e9f
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-03-01 15:36:11 -0300

    configuration default set ident

--- src/repository/repository.configuration-challenge.ts
+++ src/repository/repository.configuration-challenge.ts
@@ -6,33 +6,29 @@ import { toCamelCase } from "../utils/general/to-camel-case"
 
 
 export const getChallengeConfiguration = async (): Promise<ConfigurationDefaultI> => {
-  return new Promise(async (resolve, reject)=> {
-    try{
-      const configuration = await ConfigurationService.getConfigurationDefault(RESOURCE.CHALLENGE)
-      return resolve(configuration)
-    }catch(error){
-      return reject(error)
-    }
-  })
+  try{
+    const configuration = await ConfigurationService.getConfigurationDefault(RESOURCE.CHALLENGE)
+    return configuration
+  }catch(error){
+    return Promise.reject(error)
+  }
 }
 
 export const setDefaultConfiguration = async (configuration: ConfigurationBody, situation: string): Promise<ConfigurationDefaultI> => {
-  return new Promise(async (resolve, reject)=> {
-    try{
-      const configurationDefault = await ConfigurationService.getConfigurationDefault(situation) 
-      const configurationCamelCase = toCamelCase(configuration)
-      configurationCamelCase.situationConfig = situation,
-      configurationCamelCase.updated = new Date() 
+  try{
+    const configurationDefault = await ConfigurationService.getConfigurationDefault(situation) 
+    const configurationCamelCase = toCamelCase(configuration)
+    configurationCamelCase.situationConfig = situation,
+    configurationCamelCase.updated = new Date() 
             
-      if(configurationDefault){
-        const configurationResp = await ConfigurationService.updateConfigurationDefault(situation, configurationCamelCase)
-        return resolve(configurationResp)
-      }
-    
-      const configurationResp = await ConfigurationService.setConfiguration(configurationCamelCase)
-      return resolve(configurationResp)
-    }catch(error){
-      return reject(error)
+    if(configurationDefault){
+      const configurationResp = await ConfigurationService.updateConfigurationDefault(situation, configurationCamelCase)
+      return configurationResp
     }
-  })
+    
+    const configurationResp = await ConfigurationService.setConfiguration(configurationCamelCase)
+    return configurationResp
+  }catch(error){
+    return Promise.reject(error)
+  }
 }

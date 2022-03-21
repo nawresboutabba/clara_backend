11c621f181f9bca01d7535760cf684b15c79f672
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-03-21 13:42:49 -0300

    baremo, second aproximation

--- src/models/baremo.ts
+++ src/models/baremo.ts
@@ -1,25 +1,32 @@
 import { Schema, model } from "mongoose";
-import { ChallengeI } from "./situation.challenges";
 import { SolutionI } from "./situation.solutions";
-import { IntegrantI } from "./integrant";
-import { SpecialistInterventionI } from "./specialist-intervention";
+import { UserI } from "./users";
 
 export interface BaremoI {
-    intervention: IntegrantI | SpecialistInterventionI,
-    situation: ChallengeI | SolutionI,
+    user: UserI
+    solution: SolutionI,
+    created: Date,
+    updated: Date,
+    status: string,
+    type: 'SPECIALIST_INTERVENTION' | 'GROUP_VALIDATOR',
     comment: string
 }
 
 const baremo = new Schema({
-  intervention: {
-    type: Schema.Types.Mixed,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
   },
-  situation: {
-    type: Schema.Types.Mixed,
+  solution: {
+    type: Schema.Types.ObjectId,
+    ref: 'Solution' 
   },
+  created: Date,
+  updated: Date,
+  type: String,
   comment: String
 })
 
-baremo.index({intervention:1, situation:1}, {unique:true})
+baremo.index({intervention:1, solution:1}, {unique:true})
 
 export default model('Baremo', baremo);

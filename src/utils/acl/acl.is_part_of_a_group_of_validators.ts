f3b8c6e0eb573254ecd623b38642e340d83778b5
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-03-27 15:55:53 -0300

    is_part_of_a_group_validator fuinction added

---
+++ src/utils/acl/acl.is_part_of_a_group_of_validators.ts
@@ -0,0 +1,32 @@
+import { COMMITTE_ROLE, ERRORS, HTTP_RESPONSE } from "../../constants";
+import RoutingError from "../../handle-error/error.routing";
+import { RequestMiddleware } from "../../middlewares/middlewares.interface";
+import { IntegrantStatusI } from "../../models/integrant";
+import IntegrantService from "../../services/Integrant.service";
+import { isCommitteMember } from "./function.is_committe_member";
+
+export async function IS_PART_OF_GROUP_VALIDATOR (req: RequestMiddleware): Promise<void> {
+  try{
+    const committe: IntegrantStatusI = await isCommitteMember(req.user)
+    if (committe.isActive && committe.role === COMMITTE_ROLE.GENERAL){
+      const integrant = await IntegrantService.getIntegrantByUser(req.user)
+      const groupValidator = integrant.groupValidator
+      if (groupValidator){
+        req.utils = {...req.utils, groupValidator}
+        req.query.groupValidatorId = groupValidator.groupValidatorId
+        return Promise.resolve()
+      }else{
+        throw (new RoutingError(
+          ERRORS.ROUTING.OPERATION_NOT_AVAILABLE,
+          HTTP_RESPONSE._500,
+        ))
+      }
+    }
+    throw new RoutingError(
+      ERRORS.ROUTING.OPERATION_NOT_AVAILABLE,
+      HTTP_RESPONSE._500,
+    )
+  }catch(error){
+    return Promise.reject(error)
+  }
+}

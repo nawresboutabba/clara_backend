7b46705030ce83cb93f5cfad62585fda7392951a
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-03-27 15:33:57 -0300

    lightuserfilter replaced in array

--- src/utils/field-filters/user.ts
+++ src/utils/field-filters/user.ts
@@ -2,6 +2,16 @@ import { UserResponse } from "../../controller/users";
 import { UserI } from "../../models/users";
 import { genericArrayAreaFilter } from "./area";
 
+export const lightUserFilter =  async (user: UserI): Promise<any>=> {
+  return {
+    user_id: user.userId,
+    username: user.username,
+    email: user.email,
+    first_name: user.firstName,
+    last_name: user.lastName,
+    points: user.points,
+  }
+}
 /**
  * User information filter. 
  * Is used by controllers for return limited information in Response action.
@@ -15,6 +25,7 @@ export const genericUserFilter = async (user: UserI): Promise<UserResponse> => {
       const { areaVisible, externalUser, active, points, firstName, lastName , email, username} = user
       const area_visible = await genericArrayAreaFilter(areaVisible)
       return {
+        user_id: user.userId,
         area_visible,
         external_user: externalUser,
         active, 
@@ -38,9 +49,9 @@ export const genericArrayUserFilter =  async (users: Array<UserI>): Promise<Arra
       return []
     }
     users.forEach(user => {
-      arrayUser.push(genericUserFilter(user))
+      arrayUser.push(lightUserFilter(user))
     })
-    await Promise
+    return await Promise
       .all(arrayUser)
       .then(result => {
         return result
@@ -51,15 +62,4 @@ export const genericArrayUserFilter =  async (users: Array<UserI>): Promise<Arra
   }catch(error){
     return Promise.reject(error)
   }
-}
-
-export const lightUserFilter =  (user: UserI): any=> {
-  return ({
-    user_id: user._id,
-    username: user.username,
-    email: user.email,
-    first_name: user.firstName,
-    last_name: user.lastName,
-    points: user.points,
-  })
 }

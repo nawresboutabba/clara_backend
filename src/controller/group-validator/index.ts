2fc81fcb4bdc556897478a34f2a218cfc135fd55
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-03-27 15:53:49 -0300

    getsolutionlinked added to controller

--- src/controller/group-validator/index.ts
+++ src/controller/group-validator/index.ts
@@ -1,9 +1,27 @@
-import { Post , Controller, Route, Body, Delete , Path, Patch, Get , Request} from 'tsoa'
-import { getAllGroupValidatorsDetails, newGroupValidator } from '../../repository/repository.group-validator';
+import { Post , Controller, Route, Body, Get, Query, Inject } from 'tsoa'
+import { GroupValidatorI } from '../../models/group-validator';
+import { UserI } from '../../models/users';
+import { getAllGroupValidatorsDetails, getSolutionsLinked, newGroupValidator } from '../../repository/repository.group-validator';
+import { LightSolutionResponse } from '../solution';
+import { UserResponse } from '../users';
 
 export interface GroupValidatorBody {
     name: string,
     integrants: string[]
+}
+
+export interface GroupValidatorQueueResponse {
+    group_validator_id: string,
+    group_validator_name: string,
+    step: string, 
+    integrants: UserResponse[],
+    queue: {
+      idea: LightSolutionResponse
+      validations?: [{
+        validator?: UserResponse,
+        done?: boolean
+      }]
+    }[]
 }
 
 @Route('/group-validator')
@@ -22,6 +40,10 @@ export default class GroupValidatorController extends Controller{
      */
     @Get()
     public async getAllGroupValidatorsDetails ():Promise<any>{
-    	return getAllGroupValidatorsDetails()
+      return getAllGroupValidatorsDetails()
+    }
+    @Get('/solution')
+    public async getSolutionsLinked(@Query() query: any, @Inject() groupValidator: GroupValidatorI, @Inject() user: UserI):Promise<GroupValidatorQueueResponse>{
+      return getSolutionsLinked(query, groupValidator)
     }
 }

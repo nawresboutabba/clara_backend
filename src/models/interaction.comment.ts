2d37c998cf748d216a96cdb8a3a61b20cb570189
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-04-05 04:59:06 -0300

    new attribute added

--- src/models/interaction.comment.ts
+++ src/models/interaction.comment.ts
@@ -5,22 +5,22 @@ import InteractionBase, { InteractionBaseI, options } from "./interaction.base";
 export interface CommentI extends InteractionBaseI{
   commentId: string
   comment: string,
-  isPrivate: boolean,
-  parent?: CommentI
+  parent?: CommentI,
+  /**
+   * GROUP | PUBLIC
+   */
+  scope: string 
 }
 
 
 const Comment = InteractionBase.discriminator('Comment',new Schema({
   commentId: String, 
   comment: String,
-  isPrivate: {
-    type: Boolean,
-    default: false
-  },
   parent: {
     type: Schema.Types.ObjectId,
     ref: 'Comment'
-  }
+  },
+  scope: String
 }, options));
 
 export default Comment

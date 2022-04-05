b742335dcd7f6dc60235571ae42f064f1ed2b732
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-04-05 04:57:59 -0300

    get comments

--- src/repository/repository.comment.ts
+++ src/repository/repository.comment.ts
@@ -1,5 +1,6 @@
 import { CommentI } from "../models/interaction.comment";
 import CommentService from "../services/Comment.service";
+import { genericArrayCommentFilter } from "../utils/field-filters/comment";
 
 /**
  * Generic Comment post
@@ -10,6 +11,28 @@ export const newComment = async (comment: CommentI): Promise<CommentI> => {
   try{
     const resp = await CommentService.newComment(comment)
     return Promise.resolve(resp)
+  }catch(error){
+    return Promise.reject(error)
+  }
+}
+
+export const getComments = async (filter: any ): Promise<any> => {
+  try{
+    const parents = await CommentService.getParentsComments(filter)
+
+    const parentsFiltered = await genericArrayCommentFilter(parents)
+
+    const allChilds = await CommentService.getChildsComments(parents)
+
+    const allChildsFiltered = await genericArrayCommentFilter(allChilds)
+
+    const complete = parentsFiltered.map(parent => {
+      const childs = allChildsFiltered.filter(child => 
+        child?.parent?.comment_id == parent.comment_id)
+      return { ...parent, childs}
+    })
+
+    return complete
   }catch(error){
     return Promise.reject(error)
   }

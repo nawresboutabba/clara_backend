e0a579103ba6405c1c2409fbe32b987a0fd334cb
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-04-06 11:12:23 -0300

    cors added

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,19 @@ const morgan = require("morgan");
 
 const swaggerDocument =require( '../swagger.json')
 // const cookieParser = require('cookie-parser');
-// const cors = require('cors');
+const cors = require('cors');
 // global.repositoryError = require('./handle-error/error.repository')
 // app.use(cors());
 // app.use(cookieParser());
+
+app.use((req, res, next) => {
+  // Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header("Access-Control-Allow-Origin", "*");
+  // Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
 
 app.use(express.json());
 

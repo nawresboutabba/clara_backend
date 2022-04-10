69d0833972bbf006afde9ff8b4d4376429952893
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-04-10 12:54:49 -0300

    version addded

--- src/controller/comment/index.ts
+++ src/controller/comment/index.ts
@@ -2,12 +2,14 @@ import { InteractionBody, InteractionResponse } from "../interaction";
 
 export interface CommentBody extends InteractionBody {
     comment: string,
+    version: string,
     scope: string,
 }
 
 export interface CommentResponse extends InteractionResponse{
     comment_id: string
     comment: string,
+    version: string, 
     scope: string,
     parent?: CommentResponse
 }

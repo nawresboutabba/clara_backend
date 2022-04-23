bec882c51dd9506e514d29f1088f4e4dafe64ccb
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-04-23 10:54:12 -0300

    new baremo service added

--- src/services/Baremo.service.ts
+++ src/services/Baremo.service.ts
@@ -4,6 +4,18 @@ import Baremo, { BaremoI } from "../models/baremo";
 import { SolutionI } from "../models/situation.solutions";
 
 const BaremoService = {
+  async newBaremo(baremo:BaremoI): Promise<BaremoI>{
+    try{
+      const resp = await Baremo.create(baremo)
+      return resp
+    }catch(error){
+      return Promise.reject(new ServiceError(
+        ERRORS.SERVICE.NEW_BAREMO,
+        HTTP_RESPONSE._500,
+        error
+      )) 
+    }
+  },
   async getAllBaremosBySolution(solution: SolutionI): Promise<Array<BaremoI>> {
     try{
       const baremos = await Baremo.find({

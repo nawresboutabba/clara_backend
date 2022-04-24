c9dcdee7bb1f1041e492512e8383a9a3ccc2a289
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-04-24 13:52:15 -0300

    tag added to comment

--- src/models/interaction.comment.ts
+++ src/models/interaction.comment.ts
@@ -1,11 +1,13 @@
 import { Schema } from "mongoose";
 import InteractionBase, { InteractionBaseI, options } from "./interaction.base";
+import { TagI } from "./tag";
 
 
 export interface CommentI extends InteractionBaseI{
   commentId: string
   comment: string,
   version: string,
+  tag: TagI,
   parent?: CommentI,
   /**
    * GROUP | PUBLIC
@@ -18,6 +20,10 @@ const Comment = InteractionBase.discriminator('Comment',new Schema({
   commentId: String, 
   comment: String,
   version: String,
+  tag:{
+    type: Schema.Types.ObjectId,
+    ref: 'Tag'
+  },
   parent: {
     type: Schema.Types.ObjectId,
     ref: 'Comment'

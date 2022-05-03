99542502ddeeac4b0ec859bb2b132e55b667e258
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2022-05-03 06:23:30 -0300

    rule added

---
+++ src/utils/acl/is_baremo_creator.ts
@@ -0,0 +1,23 @@
+import { ERRORS, HTTP_RESPONSE } from "../../constants"
+import RoutingError from "../../handle-error/error.routing"
+import { RequestMiddleware } from "../../middlewares/middlewares.interface"
+import { BaremoI } from "../../models/baremo"
+import BaremoService from "../../services/Baremo.service"
+
+export async function IS_BAREMO_CREATOR (req: RequestMiddleware): Promise<void> {
+  try{
+    const baremo: BaremoI = await BaremoService.getBaremoById(req.params.baremoId)
+    if (baremo){
+      if (baremo.user.userId == req.user.userId){
+        req.utils = {...req.utils, baremo}
+        return Promise.resolve()
+      }
+    }
+    throw new RoutingError(
+      ERRORS.ROUTING.GET_BAREMO,
+      HTTP_RESPONSE._404
+    )
+  }catch(error){
+    return Promise.reject(error)
+  }
+}

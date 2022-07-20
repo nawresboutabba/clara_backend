887f94c71265809042c9bbc3640bb45aa7b40b88
= <=>
2022-07-19 23:23:57 -0300

    fix: error with builded project when mailing
    the files paths on dev works,
    but when the project is builded for production,
    we need to pick the file from the src

--- src/utils/templates-mailing/index.ts
+++ src/utils/templates-mailing/index.ts
@@ -1,18 +1,25 @@
 import { MAILING_TEMPLATES } from "../../constants";
-import * as path from 'path';
-import  { readFile } from 'fs';
-import  { promisify } from 'util'
+import * as path from "path";
+import { readFile } from "fs/promises";
 
 const fileNames = [
   MAILING_TEMPLATES.EXTERNAL_OPINION_INVITATION,
-  MAILING_TEMPLATES.NEW_EXTERNAL_USER
+  MAILING_TEMPLATES.NEW_EXTERNAL_USER,
 ];
 
 export default async function () {
   const templates = {};
   for (const templateName of fileNames) {
-    const templateBuffer = await promisify(readFile)(path.join(__dirname, `./${templateName}`));
-    const template = templateBuffer.toString('utf-8');
+    const templateBuffer = await readFile(
+      path.join(
+        process.cwd(),
+        "src",
+        "utils",
+        "templates-mailing",
+        templateName
+      )
+    );
+    const template = templateBuffer.toString("utf-8");
     templates[templateName] = template;
   }
 

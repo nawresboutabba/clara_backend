1416a37c301f252a0e25bd22e4ee6415aeda6701
= <=>
2022-08-17 16:58:08 -0300

    fix(invitation): serialization need challenge

--- src/utils/field-filters/invitation.ts
+++ src/utils/field-filters/invitation.ts
@@ -2,43 +2,37 @@ import { SolutionInvitationI } from "../../models/invitation";
 import { lightSolutionFilter } from "./solution";
 import { lightUserFilter } from "./user";
 
-export const genericSolutionInvitationFilter = async (invitation: SolutionInvitationI): Promise<any> => {
-  try{
-    const {creationDate, decisionDate, invitationAccepted, type, invitationId, resource, status} = invitation
-    const to = await lightUserFilter(invitation.to)
-    const from = await lightUserFilter(invitation.from)
-    const solution = await lightSolutionFilter(invitation.solution)
-    return {
-      resource,
-      invitation_id: invitationId,
-      creation_date: creationDate,
-      decision_date: decisionDate,
-      invitation_accepted: invitationAccepted,
-      type,
-      to,
-      from,
-      solution,
-      status
-    }
-  }catch(error){
-    return Promise.reject(error)
-  }
-}
+export const genericSolutionInvitationFilter = async (
+  invitation: SolutionInvitationI
+): Promise<any> => {
+  const {
+    creationDate,
+    decisionDate,
+    invitationAccepted,
+    type,
+    invitationId,
+    resource,
+    status,
+  } = invitation;
+  const to = await lightUserFilter(invitation.to);
+  const from = await lightUserFilter(invitation.from);
+  const solution = await lightSolutionFilter(invitation.solution);
+  return {
+    resource,
+    invitation_id: invitationId,
+    creation_date: creationDate,
+    decision_date: decisionDate,
+    invitation_accepted: invitationAccepted,
+    type,
+    to,
+    from,
+    solution,
+    status,
+  };
+};
 
-export const genericArraySolutionInvitationFilter = async (invitations: SolutionInvitationI[]): Promise<any> => {
-  try{
-    const arrayInvitations: Array<Promise<SolutionInvitationI>>= []
-    invitations.forEach(invitation => {
-      arrayInvitations.push(genericSolutionInvitationFilter(invitation))
-    })
-    return await Promise.all(arrayInvitations)
-      .then(result => {
-        return result
-      })
-      .catch(error=> {
-        return Promise.reject(error)
-      })  
-  }catch(error){
-    return Promise.reject(error)
-  }
-}
+export const genericArraySolutionInvitationFilter = async (
+  invitations: SolutionInvitationI[]
+): Promise<any> => {
+  return Promise.all(invitations.map(genericSolutionInvitationFilter));
+};

42bc2f5d0e5d41aa9dbddec2084f0c10f8cefbb9
= <=>
2022-08-30 00:23:20 -0300

    refactor: typo status

--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,74 @@
-'use strict';
-import 'module-alias/register'
-import 'dotenv/config';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+"use strict";
+import "module-alias/register";
+import "dotenv/config";
 
-import express from 'express';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import cors from "cors";
 
-import swaggerDocument from '../swagger.json';
+import swaggerDocument from "../swagger.json";
+
+import solutionsRouter from "./routes/solutions";
+import userRouter from "./routes/users";
+import companyRouter from "./routes/company";
+import areaRouter from "./routes/area";
+import groupValidatorRouter from "./routes/group-validator";
+import integrantRouter from "./routes/integrant";
+import teamRouter from "./routes/team";
+import imageRouter from "./routes/image-service";
+import visitRouter from "./routes/visit";
+import tagRouter from "./routes/tag";
+import emailRouter from "./routes/email";
+
+import { logError } from "./handle-error/middleware.log-error";
+import { clientErrorHandler } from "./handle-error/middleware.client-error-handler";
+import { errorHandler } from "./handle-error/middleware.error-handler";
+import { challengeRouter } from "./routes/challenge/new_index";
+import swaggerUi from "swagger-ui-express";
+import { parseQueryString } from "./utils/express/query-string";
+
+mongoose
+  .connect(
+    `mongodb+srv://dev-enviroment:0Q5ryUinCQ0pOeiT@pinc-se.ni0pt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    {}
+  )
+  .then((db) =>
+    console.log(
+      `DB is conected to, server: ${db.connection.host}, puerto: ${db.connection.port}, db: ${db.connection.name}`
+    )
+  )
+  .catch((err) => console.log(err));
 
 const app = express();
 
-// const cookieParser = require('cookie-parser');
-// global.repositoryError = require('./handle-error/error.repository')
-app.use(cors());
-// app.use(cookieParser());
-
-// app.use((req, res, next) => {
-//   // Qual site tem permissão de realizar a conexão, no exemplo abaixo está o '*' indicando que qualquer site pode fazer a conexão
-//   res.header('Access-Control-Allow-Origin', '*');
-//   // Quais são os métodos que a conexão pode realizar na API
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//   );
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   // app.use(cors());
-//   next();
-// });
-
+app.set("query parser", parseQueryString);
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev'));
-import solutionsRouter from './routes/solutions'
-import userRouter from './routes/users'
-import challengeRouter from './routes/challenge';
-import companyRouter from './routes/company';
-import areaRouter from './routes/area';
-import groupValidatorRouter from './routes/group-validator';
-import integrantRouter from './routes/integrant';
-import teamRouter from './routes/team'
-import imageRouter from './routes/image-service';
-import visitRouter from './routes/visit';
-import tagRouter from './routes/tag';
-import emailRouter from './routes/email';
+app.use(cors());
+app.use(morgan("dev"));
 
-// import session from './middlewares/session'
+app.use("/", solutionsRouter);
+app.use("/", userRouter);
+app.use(challengeRouter);
+app.use("/", companyRouter);
+app.use("/", areaRouter);
+app.use("/", groupValidatorRouter);
+app.use("/", integrantRouter);
+app.use("/", teamRouter);
+app.use("/", imageRouter);
+app.use("/", visitRouter);
+app.use("/", tagRouter);
+app.use("/", emailRouter);
 
-import { logError } from './handle-error/middleware.log-error';
-import { clientErrorHandler } from './handle-error/middleware.client-error-handler';
-import { errorHandler } from './handle-error/middleware.error-handler';
-
-import swaggerUi = require('swagger-ui-express');
+app.use(logError);
+app.use(clientErrorHandler);
+app.use(errorHandler);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT ?? 3000;
-/* const DB_CONNECTION = 'localhost:27017'
-const DB_NAME = 'PINC-SE' */
-
-mongoose.connect(`mongodb+srv://dev-enviroment:0Q5ryUinCQ0pOeiT@pinc-se.ni0pt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
-})
-  .then(db => console.log(`DB is conected to, server: ${db.connection.host}, puerto: ${db.connection.port}, db: ${db.connection.name}`))
-  .catch(err => console.log(err))
-
 
 app.listen(PORT, () =>
-  console.log(
-    `¡Aplicación de ejemplo escuchando en el puerto ${PORT}`
-  )
+  console.log(`¡Aplicación de ejemplo escuchando en el puerto ${PORT}`)
 );
-// app.use(session)
-app.use('/', solutionsRouter);
-app.use('/',userRouter);
-app.use('/', challengeRouter);
-app.use('/', companyRouter);
-app.use('/', areaRouter);
-app.use('/', groupValidatorRouter);
-app.use('/', integrantRouter);
-app.use('/', teamRouter);
-app.use('/', imageRouter);
-app.use('/', visitRouter);
-app.use('/', tagRouter);
-app.use('/', emailRouter);
-app.use(logError)
-app.use(clientErrorHandler)
-app.use(errorHandler)
-/* if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'testing' || process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production') {
-    app.use();
-}
-
- */
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
--- src/utils/sanitization/thirdDifficulty.check.ts
+++ src/utils/sanitization/thirdDifficulty.check.ts
@@ -1,27 +1,32 @@
-import * as assert from 'assert';
-import { SOLUTION_STATUS } from "../../constants"
-import { SolutionI } from '../../models/situation.solutions';
-import SolutionStateMachine from '../state-machine/state-machine.solution';
+import * as assert from "assert";
+import { SOLUTION_STATUS } from "../../constants";
+import { SolutionI } from "../../models/situation.solutions";
+import SolutionStateMachine from "../state-machine/state-machine.solution";
 
-export function checkThirdDifficulty(body:CallableFunction):any{
-
-  return body('transition').custom(async (value: string, { req }): Promise<void>=> {
-    try{
-      const solution:SolutionI = req.resources.solution
-      const transition = await SolutionStateMachine.dispatch(solution.status , value)
-      if (transition== SOLUTION_STATUS.PROPOSED){
-        /**
-         * Is not required
-         */
-        return Promise.resolve()
-      }else if (transition == SOLUTION_STATUS.DRAFT){
-        return Promise.resolve()
-      }else if (transition == SOLUTION_STATUS.APROVED_FOR_DISCUSSION){
-        return Promise.resolve()
+export function checkThirdDifficulty(body: CallableFunction): any {
+  return body("transition").custom(
+    async (value: string, { req }): Promise<void> => {
+      try {
+        const solution: SolutionI = req.resources.solution;
+        const transition = await SolutionStateMachine.dispatch(
+          solution.status,
+          value
+        );
+        if (transition == SOLUTION_STATUS.PROPOSED) {
+          /**
+           * Is not required
+           */
+          return Promise.resolve();
+        } else if (transition == SOLUTION_STATUS.DRAFT) {
+          return Promise.resolve();
+        } else if (transition == SOLUTION_STATUS.APPROVED_FOR_DISCUSSION) {
+          return Promise.resolve();
+        }
+        throw "Transition not Implemented";
+      } catch (error) {
+        error.message = `third_difficulty: ${error.message}`;
+        return Promise.reject(error);
       }
-      throw 'Transtition not Implemented'
-    }catch(error){
-      error.message = `third_difficulty: ${error.message}`
-      return Promise.reject(error)
     }
-  })}
+  );
+}

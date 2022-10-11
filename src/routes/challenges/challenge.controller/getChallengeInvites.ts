17981b3520d90598c6d1cf44300bec6f863f86ff
vensauro <ivens.joris.nogueira@gmail.com>
2022-10-11 11:38:55 -0300

    feat: get challenge invites

---
+++ src/routes/challenges/challenge.controller/getChallengeInvites.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { INVITATION_STATUS } from "../../../models/invitation";
+import { UserI } from "../../../models/users";
+import InvitationService from "../../../services/Invitation.service";
+import { validate } from "../../../utils/express/express-handler";
+import { genericArrayChallengeInvitationFilter } from "../../../utils/field-filters/invitation";
+import { removeEmpty } from "../../../utils/general/remove-empty";
+import * as ChallengeRep from "../challenges.repository"
+
+export const getChallengeInvites = validate(
+  {
+    params: z.object({ challengeId: z.string() }),
+    query: z.object({ status: z.nativeEnum(INVITATION_STATUS) }).partial(),
+  },
+  async ({ user, params: { challengeId }, query: { status } }, res) => {
+    const challenge = await ChallengeRep.getChallengeById(challengeId);
+    if (
+      challenge.author.userId !== user.userId &&
+      challenge.coauthor.every((e: UserI) => e.userId !== user.userId)
+    ) {
+      return res.status(403).json({ message: "not authorized" });
+    }
+
+    const invitations = await InvitationService.getChallengeInvitations(
+      removeEmpty({
+        resource: challenge,
+        status,
+      })
+    );
+
+    return genericArrayChallengeInvitationFilter(invitations);
+  }
+);

2bdbceb45e3d4d30c52004463dbfdb246c1537c0
vensauro <ivens.joris.nogueira@gmail.com>
2022-10-11 11:39:13 -0300

    feat: response challenge invite

---
+++ src/routes/challenges/challenge.controller/responseChallengeInvite.ts
@@ -0,0 +1,52 @@
+import { z } from "zod";
+import { ChallengeInvitation, INVITATION_STATUS, INVITATION_TYPE } from "../../../models/invitation";
+import Challenge from "../../../models/situation.challenges";
+import { validate } from "../../../utils/express/express-handler";
+import { genericChallengeInvitationFilter } from "../../../utils/field-filters/invitation";
+import { getCurrentDate } from "../../../utils/general/date";
+
+export const responseChallengeInvite = validate(
+  {
+    params: z.object({ challengeId: z.string(), invitationId: z.string() }),
+    body: z.object({
+      response: z.enum([
+        INVITATION_STATUS.ACCEPTED,
+        INVITATION_STATUS.REJECTED,
+      ]),
+    }),
+  },
+  async (
+    { user, params: { invitationId, challengeId }, body: { response } },
+    res
+  ) => {
+    const invite = await ChallengeInvitation.findById(invitationId)
+      .populate("resource")
+      .populate("from")
+      .populate("to");
+
+    if (invite.to.userId !== user.userId) {
+      return res.status(403).json({ message: "not authorized" });
+    }
+    if (invite.status !== INVITATION_STATUS.PENDING) {
+      return res
+        .status(403)
+        .json({ message: "The invitation cannot be answered" });
+    }
+
+    if (response === INVITATION_STATUS.ACCEPTED) {
+      await Challenge.findByIdAndUpdate(
+        challengeId,
+        {
+          $addToSet:
+            invite.type === INVITATION_TYPE.EXTERNAL_OPINION
+              ? { externalOpinion: user }
+              : { coauthor: user },
+        }
+      );
+    }
+
+    invite.decisionDate = getCurrentDate();
+    invite.status = response;
+    return genericChallengeInvitationFilter(await invite.save());
+  }
+);

b7d59157b8ab8ecc71777ba5a58c54e20a9d1e9d
vensauro <ivens.joris.nogueira@gmail.com>
2022-11-01 17:53:29 -0300

    refactor: solutions url with restfull pattern

--- src/routes/solutions/solution.route.ts
+++ src/routes/solutions/solution.route.ts
@@ -8,30 +8,30 @@ export const solutionsRouter = express.Router();
 solutionsRouter
   .use(authentication)
   .use(oldRoute)
-  .get("/solution", controller.getSolutions)
-  .get("/solution/:solutionId", controller.getSolution)
-  .patch("/solution/:solutionId", controller.updateChallenge)
-  .delete("/solution/:solutionId", controller.deleteSolution)
-  .post("/solution/:solutionId/transition", controller.changeSolutionState)
-  .post("/solution/:solutionId/author", controller.changeSolutionAuthor)
-  .post("/solution/:solutionId/leave", controller.leaveSolution)
-  .get("/solution/:solutionId/comment", controller.getSolutionComments)
+  .get("/solutions", controller.getSolutions)
+  .get("/solutions/:solutionId", controller.getSolution)
+  .patch("/solutions/:solutionId", controller.updateChallenge)
+  .delete("/solutions/:solutionId", controller.deleteSolution)
+  .post("/solutions/:solutionId/transition", controller.changeSolutionState)
+  .post("/solutions/:solutionId/author", controller.changeSolutionAuthor)
+  .post("/solutions/:solutionId/leave", controller.leaveSolution)
+  .get("/solutions/:solutionId/comment", controller.getSolutionComments)
   .get(
-    "/solution/:solutionId/comment/resume",
+    "/solutions/:solutionId/comment/resume",
     controller.getSolutionCommentsResume
   )
   .get(
-    "/solution/:solutionId/comment/:commentId",
+    "/solutions/:solutionId/comment/:commentId",
     controller.getSolutionComment
   )
-  .post("/solution/:solutionId/comment", controller.createSolutionComment)
-  .post("/solution/:solutionId/invitation", controller.createSolutionInvite)
-  .get("/solution/:solutionId/invitation", controller.getSolutionInvites)
+  .post("/solutions/:solutionId/comment", controller.createSolutionComment)
+  .post("/solutions/:solutionId/invitation", controller.createSolutionInvite)
+  .get("/solutions/:solutionId/invitation", controller.getSolutionInvites)
   .post(
-    "/solution/:solutionId/invitation/:invitationId/response",
+    "/solutions/:solutionId/invitation/:invitationId/response",
     controller.responseSolutionInvite
   )
   .post(
-    "/solution/:solutionId/invitation/:invitationId/cancel",
+    "/solutions/:solutionId/invitation/:invitationId/cancel",
     controller.cancelSolutionInvite
   );
